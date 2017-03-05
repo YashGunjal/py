5601f0406c88f1eb9eced5b79789e4a41f0b4694
jack <yashgunjal98@gmail.com>
2017-03-05 23:33:17 +0530

    added  avg.py

---
+++ avg.py
@@ -0,0 +1,12 @@
+sum=0.0
+count=0
+num=input("enter the number")
+while num !=-1:
+    sum=sum+num
+    count += 1
+    num =input("enter your number:")
+print "average is",sum/count
+#standard deviation tells you how the values are scattered--a small value
+#means they are clustered together and a large value indicates they are
+#widely separated.  
+'
